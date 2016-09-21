b14ddedefa43146df9ec1ee385f09d335f4a51b0
lxs <lxs_bupt@126.com>
2016-09-21 22:56:30 +0800

    Initial commit

---
+++ app/src/main/java/com/lxs/customview/MainActivity.java
@@ -0,0 +1,23 @@
+package com.lxs.customview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    RatioCircleView mRatioCircleView;
+    ArrayList<RatioCircleData> mRatioCircleDatas;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mRatioCircleView = (RatioCircleView) findViewById(R.id.ratio_circle_view);
+        mRatioCircleDatas = new ArrayList<>();
+        for (int i = 0; i < 6; i++) {
+            mRatioCircleDatas.add(new RatioCircleData(i + "", i));
+        }
+        mRatioCircleView.setData(mRatioCircleDatas);
+    }
+}
---
+++ app/src/main/java/com/lxs/customview/RatioCircleData.java
@@ -0,0 +1,66 @@
+package com.lxs.customview;
+
+import android.support.annotation.NonNull;
+
+/**
+ * 实现功能：
+ * 作    者：xinsheng.lv
+ * 创建日期：2016年09月21日 22:33
+ * 修 改 者：修改日期：修改内容：
+ */
+public class RatioCircleData {
+    // 用户关心数据
+    private String name;        // 名字
+    private float value;        // 数值
+    private float percentage;   // 百分比
+
+    // 非用户关心数据
+    private int color = 0;      // 颜色
+    private float angle = 0;    // 角度
+
+    public RatioCircleData(@NonNull String name, @NonNull float value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getValue() {
+        return value;
+    }
+
+    public void setValue(float value) {
+        this.value = value;
+    }
+
+    public float getPercentage() {
+        return percentage;
+    }
+
+    public void setPercentage(float percentage) {
+        this.percentage = percentage;
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+    }
+
+    public float getAngle() {
+        return angle;
+    }
+
+    public void setAngle(float angle) {
+        this.angle = angle;
+    }
+
+}
